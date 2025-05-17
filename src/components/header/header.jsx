ee0a5c6ae4b79e2240677bf7e419d2bf34989dce
Mathis <maureau.mathis@gmail.com>
2025-05-17 10:42:28 +0200

    logo header

--- src/components/header/header.jsx
+++ src/components/header/header.jsx
@@ -1,11 +1,12 @@
 import './header.css';
 import SearchBar from '../searchBar/searchBar';
+import logo from '../../assets/Logo.png';
 
 const Header = () => {
     return (
         <header className="header">
             <div className="header-left">
-                <img src="/src/assets/Logo.png" alt="Logo Artheca" className="header-logo" />
+                <img src={logo} alt="Logo Artheca" className="header-logo" />
                 <h1 className="header-title">Artheca</h1>
             </div>
             <SearchBar />
